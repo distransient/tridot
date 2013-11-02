5f413b667eb50ec0a6fd77fd8e62270f58007190
humanoidanalog <humanoidanalog@gmail.com>
2013-11-02 12:47:51 -0700

    added load bar, began defining functions

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-//
+//check for animation prefix
+var requestAnimationFrame = window.requestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame ||
+                            return -1;
+
+3dot = {};
+3dot.Dot = {};
+3dot.json = function(url) {
+  var request = new XMLHttpRequest();
+  request.open("Get", url, false);
+  request.send(null);
+  return this.parse(request.responseText);
+}
+3dot.parse = function(json) {
+
+};
+
+(function() {
+  3dot.update();
+  requestAnimationFrame(this);
+}() );
